ff79b901a599214ee7fdd7979b49e5e543b33eab
shixia <1498047434@qq.com>
2021-06-05 17:30:15 +0800

    v6.1 配置文件修改为yml格式，增加自定义配置项 自定义配置项读取方式：@Value(${配置名[:][默认配置值]})

--- src/main/java/cn/edkso/wiki/controller/TestController.java
+++ src/main/java/cn/edkso/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.edkso.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    //@Value(${配置名[:][默认配置值]})
+    @Value("${myenv.name:亿迪}")
+    private String name;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + name;
     }
 
     @PostMapping("/hello/post")
