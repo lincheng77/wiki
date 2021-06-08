35cb4ccda536f9c155c5cd777bd5408e195b5a93
shixia <1498047434@qq.com>
2021-06-08 19:44:33 +0800

    v7.1 vue增加Ant Design of Vue；新增button示例

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <a-button type="danger">
+      Danger
+    </a-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
