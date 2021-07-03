ee83bba101ae1af63f03b3f18cb85b5864b7495e
dingmengdi-rui-7000 <1498047434@qq.com>
2021-07-03 21:36:09 +0800

    v7.5 （7.4提交并没有生效） ①增加the-footer.vue ②增加the-header.vue

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center' }">
+    Ant Design ©2018 Created by Ant UED
+  </a-layout-footer>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default {
+  name: "the-footer",
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<style scoped>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+
+</style>
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,44 @@
+<template>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+          theme="dark"
+          mode="horizontal"
+          v-model:selectedKeys="selectedKeys1"
+          :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default {
+  name: "the-header",
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<style scoped>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+
+</style>
