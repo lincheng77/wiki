db314bb6793e3022c070fae8d89a69d43e90b763
dingmengdi-rui-7000 <1498047434@qq.com>
2021-07-04 23:55:19 +0800

    v8.0.fix 目前官网组件实例有问题，全部重来覆盖前面所做工作，具体工作内容是一致的

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,34 +1,15 @@
 <template>
-  <a-layout-footer :style="{ textAlign: 'center' }">
+  <a-layout-footer style="text-align: center">
     Ant Design ©2018 Created by Ant UED
   </a-layout-footer>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
-export default {
-  name: "the-footer",
-  props: {
-    msg: String,
-  },
-}
+<script lang="ts">
+import { defineComponent} from "vue";
+export default defineComponent({
+  name: 'the-footer'
+})
 </script>
 
 <style scoped>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
-
 </style>
