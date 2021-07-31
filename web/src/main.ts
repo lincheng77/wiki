75016c1121d59386ade94e2453a3fe3fbfadc57d
dingmengdi-rui-7000 <1498047434@qq.com>
2021-07-31 21:20:30 +0800

    v8.1 ①集成图标组件 ②修改the-footer界面显示内容

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import * as Icons from '@ant-design/icons-vue'
 
-import axios from 'axios';
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
 
-
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+// 全局使用 Ant Design 的图标库
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
