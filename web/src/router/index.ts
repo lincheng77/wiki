201a1476099af0e32b917f483cf55d49d6c47c42
dingmengdi-rui-7000 <1498047434@qq.com>
2021-08-04 21:04:41 +0800

    v9.0 ①将about.vue、home.vue 页面改为小写字母开头 ②about页面懒加载（编译）改为运行时编译

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,12 +9,13 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/About',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
