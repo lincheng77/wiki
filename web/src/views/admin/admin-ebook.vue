84b8fe949eb687572c60e27f1d7cdb56ea5f8fec
dingmengdi-rui-7000 <1498047434@qq.com>
2021-08-09 01:15:47 +0800

    v9.10 模态框显示电子书表单，编辑某一行时可以将该行数据显示到表单

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -13,7 +13,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary" @click="edit">
+            <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
             <a-button type="danger">
@@ -25,8 +25,29 @@
     </a-layout-content>
   </a-layout>
 
-  <a-modal title="电子书表单" v-model:visible="modalVisible" :confirm-loading="modalLoading" @ok="handleModalOk">
-    <p>test</p>
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id" />
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id" />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.desc" type="textarea" />
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
@@ -113,6 +134,7 @@ export default defineComponent({
     };
 
     //--------------------------------------表单------------------------------------------------------
+    const ebook = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
@@ -125,8 +147,9 @@ export default defineComponent({
     /**
      * 编辑
      */
-    const edit = () => {
+    const edit = (record) => {
       modalVisible.value = true;
+      ebook.value = record
     };
 
     //--------------------------------------------onMounted return--------------------------------------------------
@@ -143,6 +166,7 @@ export default defineComponent({
       loading,
       handleTableChange,
       edit,
+      ebook,
       modalVisible,
       modalLoading,
       handleModalOk
